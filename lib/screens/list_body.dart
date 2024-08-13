35696763fbe4cd3787d7608440158b699feb4746
finteger <nashassociatesinc@gmail.com>
2024-08-13 15:23:58 -0600

    Update

---
+++ lib/screens/list_body.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+List<String> categories = [
+  'Category 1',
+  'Category 2',
+  'Category 3',
+  'Category 4',
+  'Category 5',
+  'Category 6',
+  'Category 7',
+  'Category 8',
+];
+
+List<Widget> numbers = [
+  Column(
+    children: [
+      for (int i = 0; i < categories.length; i++)
+        SizedBox(
+          height: 200,
+          width: 400,
+          child: Card(
+            surfaceTintColor: Colors.blueGrey,
+            //color: Colors.grey,
+            child: Center(
+                child: Text(
+              categories[i],
+              style: TextStyle(fontSize: 30),
+            )),
+          ),
+        ),
+    ],
+  ),
+];
