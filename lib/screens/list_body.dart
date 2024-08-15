86284a1eeb1915c3db649b64b81adaa6b3effbd1
legaspigladdies6@gmail.com <legaspigladdies6@gmail.com>
2024-08-15 11:40:19 -0700

    comment

--- lib/screens/list_body.dart
+++ lib/screens/list_body.dart
@@ -1,33 +1,30 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
-List<String> categories = [
-  'Category 1',
-  'Category 2',
-  'Category 3',
-  'Category 4',
-  'Category 5',
-  'Category 6',
-  'Category 7',
-  'Category 8',
+List<String> photos = [
+  './assets/images/code1.jpg',
+  './assets/images/code2.jpg',
+  './assets/images/code3.jpg',
+  './assets/images/code4.jpg',
+  './assets/images/code5.jpg',
+  './assets/images/code6.jpg',
+  './assets/images/code7.jpg',
+  './assets/images/code8.jpg',
 ];
 
 List<Widget> numbers = [
   Column(
     children: [
-      for (int i = 0; i < categories.length; i++)
+      for (int i = 0; i < photos.length; i++)
         SizedBox(
           height: 200,
           width: 400,
           child: Card(
-            surfaceTintColor: Colors.blueGrey,
-            //color: Colors.grey,
-            child: Center(
-                child: Text(
-              categories[i],
-              style: TextStyle(fontSize: 30),
-            )),
-          ),
+              surfaceTintColor: Colors.blueGrey[200],
+              child: Image(
+                fit: BoxFit.cover,
+                image: AssetImage(photos[i]),
+              )),
         ),
     ],
   ),
